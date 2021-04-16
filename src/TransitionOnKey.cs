936e4e5be08ced0dd923fa18942cc3802ad8d070
Inspiaaa <lavaafterburner@gmail.com>
2021-04-16 12:01:01 +0200

    Add new transition types for key press events
    Create four new transition types that can trigger on key presses / releases / ... They can make transitions for keyboard events more readable and easier

---
+++ src/TransitionOnKey.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FSM {
+    public static class TransitionOnKey {
+        
+        public class Down : TransitionBase {
+            private KeyCode keyCode;
+
+            public Down(
+                    string from, 
+                    string to, 
+                    KeyCode key,
+                    bool forceInstantly = false) : base(from, to, forceInstantly) {
+
+                keyCode = key;
+            }
+
+            public override bool ShouldTransition() {
+                return Input.GetKey(keyCode);
+            }
+        }
+
+        public class Release : TransitionBase {
+            private KeyCode keyCode;
+
+            public Release(
+                    string from, 
+                    string to, 
+                    KeyCode key,
+                    bool forceInstantly = false) : base(from, to, forceInstantly) {
+
+                keyCode = key;
+            }
+
+            public override bool ShouldTransition() {
+                return Input.GetKeyUp(keyCode);
+            }
+        }
+
+        public class Press : TransitionBase {
+            private KeyCode keyCode;
+
+            public Press(
+                    string from, 
+                    string to, 
+                    KeyCode key,
+                    bool forceInstantly = false) : base(from, to, forceInstantly) {
+
+                keyCode = key;
+            }
+
+            public override bool ShouldTransition() {
+                return Input.GetKeyDown(keyCode);
+            }
+        }
+
+        public class Up : TransitionBase {
+            private KeyCode keyCode;
+
+            public Up(
+                    string from, 
+                    string to, 
+                    KeyCode key,
+                    bool forceInstantly = false) : base(from, to, forceInstantly) {
+
+                keyCode = key;
+            }
+
+            public override bool ShouldTransition() {
+                return ! Input.GetKey(keyCode);
+            }
+        }
+    }
+}
