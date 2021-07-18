2801de89953a567d386318d1c3af223f1e673f0d
KOGA Mitsuhiro <shiena.jp@gmail.com>
2021-07-18 20:14:36 +0900

    add playable sample scene

--- Samples~/PlayerController.cs
+++ Samples~/PlayerController.cs
@@ -2,11 +2,15 @@ using UnityEngine;
 
 namespace FSM.Samples
 {
+    [RequireComponent(typeof(Rigidbody))]
     public class PlayerController : MonoBehaviour
     {
         public static PlayerController Instance { get; private set; }
+        [SerializeField] private float speed = 2;
+        private Rigidbody rb;
+        private Vector3 movement;
 
-        void Awake()
+        private void Awake()
         {
             if (Instance != null && Instance != this)
             {
@@ -19,7 +23,31 @@ namespace FSM.Samples
             }
         }
 
-        void OnDestroy()
+        private void Start()
+        {
+            rb = GetComponent<Rigidbody>();
+        }
+
+        private void Update()
+        {
+            movement = new Vector3(
+                Input.GetAxis("Horizontal"),
+                0f,
+                Input.GetAxis("Vertical")
+            );
+        }
+
+        private void FixedUpdate()
+        {
+            Moving();
+        }
+
+        private void Moving()
+        {
+            rb.velocity = movement * speed;
+        }
+
+        private void OnDestroy()
         {
             if (Instance == this)
             {
