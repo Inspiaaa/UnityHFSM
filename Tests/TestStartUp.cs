d1a59c0472c88d20d416987e9b62da0c24728194
Inspiaaa <lavaafterburner@gmail.com>
2021-12-05 01:01:41 +0100

    Make code style of tests consistent
    - Switch indentation to tabs
    - Use idiomatic braces { } placement

--- Tests/TestStartUp.cs
+++ Tests/TestStartUp.cs
@@ -6,73 +6,80 @@ using FSM;
 
 namespace FSM.Tests
 {
-    public class TestStartUp
-    {
-        private Recorder recorder;
-        private StateMachine fsm;
+	public class TestStartUp
+	{
+		private Recorder recorder;
+		private StateMachine fsm;
 
-        [SetUp]
-        public void Setup() {
-            recorder = new Recorder();
-            fsm = new StateMachine();
-        }
+		[SetUp]
+		public void Setup()
+		{
+			recorder = new Recorder();
+			fsm = new StateMachine();
+		}
 
-        [Test]
-        public void TestStartupEvents() {
-            fsm.AddState("A", recorder.Track(new State()));
-            recorder.Check.Empty();
+		[Test]
+		public void TestStartupEvents()
+		{
+			fsm.AddState("A", recorder.Track(new State()));
+			recorder.Check.Empty();
 
-            fsm.Init();
-            recorder.Check.Enter("A").All();
-            Assert.AreEqual("A", fsm.ActiveStateName);
+			fsm.Init();
+			recorder.Check.Enter("A").All();
+			Assert.AreEqual("A", fsm.ActiveStateName);
 
-            fsm.OnLogic();
-            recorder.Check.Logic("A").All();
-        }
+			fsm.OnLogic();
+			recorder.Check.Logic("A").All();
+		}
 
-        [Test]
-        public void TestImplicitStartState() {
-            fsm.AddState("A");
-            fsm.AddState("B");
-            fsm.Init();
-            Assert.AreEqual("A", fsm.ActiveStateName);
-        }
+		[Test]
+		public void TestImplicitStartState()
+		{
+			fsm.AddState("A");
+			fsm.AddState("B");
+			fsm.Init();
+			Assert.AreEqual("A", fsm.ActiveStateName);
+		}
 
-        [Test]
-        public void TestExplicitStartState() {
-            fsm.AddState("A");
-            fsm.AddState("B");
-            fsm.SetStartState("B");
-            fsm.Init();
-            Assert.AreEqual("B", fsm.ActiveStateName);
-        }
+		[Test]
+		public void TestExplicitStartState()
+		{
+			fsm.AddState("A");
+			fsm.AddState("B");
+			fsm.SetStartState("B");
+			fsm.Init();
+			Assert.AreEqual("B", fsm.ActiveStateName);
+		}
 
-        [Test]
-        public void TestExplicitStartStateBeforeAdding() {
-            fsm.SetStartState("B");
-            fsm.AddState("A");
-            fsm.AddState("B");
-            fsm.Init();
-            Assert.AreEqual("B", fsm.ActiveStateName);
-        }
+		[Test]
+		public void TestExplicitStartStateBeforeAdding()
+		{
+			fsm.SetStartState("B");
+			fsm.AddState("A");
+			fsm.AddState("B");
+			fsm.Init();
+			Assert.AreEqual("B", fsm.ActiveStateName);
+		}
 
-        [Test]
-        public void TestSetStartStateWhileRunning() {
-            fsm.AddState("A");
-            fsm.AddState("B");
-            fsm.Init();
+		[Test]
+		public void TestSetStartStateWhileRunning()
+		{
+			fsm.AddState("A");
+			fsm.AddState("B");
+			fsm.Init();
 
-            fsm.OnLogic();
-            fsm.SetStartState("B");
-            fsm.OnLogic();
-            Assert.AreEqual("A", fsm.ActiveStateName);
-        }
+			fsm.OnLogic();
+			fsm.SetStartState("B");
+			fsm.OnLogic();
+			Assert.AreEqual("A", fsm.ActiveStateName);
+		}
 
-        [Test]
-        public void TestNotInitializedException() {
-            fsm.AddState("A");
-            StateBase<string> activeState;
-            Assert.Throws<FSM.Exceptions.StateMachineNotInitializedException>(() => activeState = fsm.ActiveState);
-        }
-    }
+		[Test]
+		public void TestNotInitializedException()
+		{
+			fsm.AddState("A");
+			StateBase<string> activeState;
+			Assert.Throws<FSM.Exceptions.StateMachineNotInitializedException>(() => activeState = fsm.ActiveState);
+		}
+	}
 }
