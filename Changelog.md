75dca26a29a7f76f8bcb03bfddc123bbd549d642
Inspiaaa <lavaafterburner@gmail.com>
2022-01-21 14:37:36 +0100

    Make the shortcut methods to extension methods
    Move the shortcut methods of the StateMachine class to a dedicated file to make the codebase cleaner

--- Changelog.md
+++ Changelog.md
@@ -2,6 +2,14 @@
 
 ---
 
+## In progress
+
+### Changed
+
+- The "shortcut methods" of the state machine have been moved to a dedicated class as extension methods. This does not change the API or usage in any way, but makes the internal code cleaner. -> This change reduces the coupling between the base StateMachine class and the State / Transition classes. Instead, the StateMachine only depends on the StateBase and TransitionBase classes. This especially shows that the extension methods are optional and not necessary in a fundamental way.
+
+---
+
 ## 1.8 - Generics
 
 Version 1.8 of UnityHFSM adds support for generics. Now the datatype of state identifiers / names and the type of event names can be easily changed. Thanks to the new "shortcut methods", state machines can be written with less boilerplate than ever and certain cases, such as empty states, can be optimised automatically for you.
@@ -11,7 +19,7 @@ Version 1.8 of UnityHFSM adds support for generics. Now the datatype of state id
 - Support for generics for the state identifiers and event names
 
 - "Shortcut methods" for reduced boilerplate and automatic optimisation
-  
+
   ```csharp
   fsm.AddState("FollowPlayer", new State(
       onLogic: s => MoveTowardsPlayer()
@@ -19,13 +27,13 @@ Version 1.8 of UnityHFSM adds support for generics. Now the datatype of state id
   // Now
   fsm.AddState("FollowPlayer", onLogic: s => MoveTowardsPlayer());
   ```
-  
+
   ```csharp
   fsm.AddState("ExtractIntel", new State());
   // Now
   fsm.AddState("ExtractIntel");
   ```
-  
+
   ```csharp
   fsm.AddTransition(new Transition("A", "B"));
   // Now
@@ -39,33 +47,33 @@ Version 1.8 of UnityHFSM adds support for generics. Now the datatype of state id
 ### Changed
 
 - The datatype of the input parameter of `onEnter` / `onLogic` / `onExit` for `State` has changed. This is due to the inheritance hierarchy and the way generic support was added to the codebase while still trying to retain the ease of use of the string versions.
-  
+
   Previously:
-  
+
   ```csharp
   void FollowPlayer(State state)
   {
       // ...
   }
-  
+
   fsm.AddState("FollowPlayer", new State(onLogic: FollowPlayer));
   ```
-  
+
   Now:
-  
+
   ```csharp
   void FollowPlayer(State<string> state)
   {
       // ...
   }
-  
+
   fsm.AddState("FollowPlayer", new State(onLogic: FollowPlayer));
   ```
 
 - States and transitions no longer carry a reference to the MonoBehaviour by default.
-  
+
   - Now the constructor of `StateMachine` does not require mono anymore => `new StateMachine()`  instead of `new StateMachine(this)`
-  
+
   - The reference to mono has to be passed into the `CoState` constructor => `new CoState(this, ...)`
 
 ### Fixed
---
+++ src/StateMachineShortcuts.cs
@@ -0,0 +1,120 @@
+using System;
+
+namespace FSM
+{
+	public static class StateMachineShortcuts
+	{
+		/*
+			"Shortcut" methods
+			- These are meant to reduce the boilerplate code required by the user for simple
+			states and transitions.
+			- They do this by creating a new State / Transition instance in the background
+			and then setting the desired fields.
+			- They can also optimise certain cases for you by choosing the best type,
+			such as a StateBase for an empty state instead of a State instance.
+		*/
+
+		/// <summary>
+		/// Shortcut method for adding a regular state.
+		/// It creates a new State() instance under the hood. => See State for more information.
+		/// For empty states with no logic it creates a new StateBase for optimal performance.
+		/// </summary>
+		public static void AddState<TOwnId, TStateId, TEvent>(
+			this StateMachine<TOwnId, TStateId, TEvent> fsm,
+			TStateId name,
+			Action<State<TStateId>> onEnter = null,
+			Action<State<TStateId>> onLogic = null,
+			Action<State<TStateId>> onExit = null,
+			Func<State<TStateId>, bool> canExit = null,
+			bool needsExitTime = false)
+		{
+			// Optimise for empty states
+			if (onEnter == null && onLogic == null && onExit == null && canExit == null)
+			{
+				fsm.AddState(name, new StateBase<TStateId>(needsExitTime));
+				return;
+			}
+
+			fsm.AddState(name, new State<TStateId>(onEnter, onLogic, onExit, canExit, needsExitTime));
+		}
+
+		/// <summary>
+		/// Creates the most efficient transition type possible for the given parameters.
+		/// It creates a Transition instance when a condition is specified and otherwise
+		/// it returns a TransitionBase.
+		/// </summary>
+		private static TransitionBase<TStateId> CreateOptimizedTransition<TStateId>(
+			TStateId from,
+			TStateId to,
+			Func<Transition<TStateId>, bool> condition = null,
+			bool forceInstantly = false)
+		{
+			if (condition == null)
+				return new TransitionBase<TStateId>(from, to, forceInstantly);
+
+			return new Transition<TStateId>(from, to, condition, forceInstantly);
+		}
+
+		/// <summary>
+		/// Shortcut method for adding a regular transition.
+		/// It creates a new Transition() instance under the hood. => See Transition for more information.
+		/// When no condition is required, it creates a TransitionBase for optimal performance.
+		/// </summary>
+		public static void AddTransition<TOwnId, TStateId, TEvent>(
+			this StateMachine<TOwnId, TStateId, TEvent> fsm,
+			TStateId from,
+			TStateId to,
+			Func<Transition<TStateId>, bool> condition = null,
+			bool forceInstantly = false)
+		{
+			fsm.AddTransition(CreateOptimizedTransition(from, to, condition, forceInstantly));
+		}
+
+		/// <summary>
+		/// Shortcut method for adding a regular transition that can happen from any state.
+		/// It creates a new Transition() instance under the hood. => See Transition for more information.
+		/// When no condition is required, it creates a TransitionBase for optimal performance.
+		/// </summary>
+		public static void AddTransitionFromAny<TOwnId, TStateId, TEvent>(
+			this StateMachine<TOwnId, TStateId, TEvent> fsm,
+			TStateId to,
+			Func<Transition<TStateId>, bool> condition = null,
+			bool forceInstantly = false)
+		{
+			fsm.AddTransition(CreateOptimizedTransition(default, to, condition, forceInstantly));
+		}
+
+		/// <summary>
+		/// Shortcut method for adding a new trigger transition between two states that is only checked
+		/// when the specified trigger is activated.
+		/// It creates a new Transition() instance under the hood. => See Transition for more information.
+		/// When no condition is required, it creates a TransitionBase for optimal performance.
+		/// </summary>
+		public static void AddTriggerTransition<TOwnId, TStateId, TEvent>(
+			this StateMachine<TOwnId, TStateId, TEvent> fsm,
+			TEvent trigger,
+			TStateId from,
+			TStateId to,
+			Func<Transition<TStateId>, bool> condition = null,
+			bool forceInstantly = false)
+		{
+			fsm.AddTriggerTransition(trigger, CreateOptimizedTransition(from, to, condition, forceInstantly));
+		}
+
+		/// <summary>
+		/// Shortcut method for adding a new trigger transition that can happen from any possible state, but is only
+		/// checked when the specified trigger is activated.
+		/// It creates a new Transition() instance under the hood. => See Transition for more information.
+		/// When no condition is required, it creates a TransitionBase for optimal performance.
+		/// </summary>
+		public static void AddTriggerTransitionFromAny<TOwnId, TStateId, TEvent>(
+			this StateMachine<TOwnId, TStateId, TEvent> fsm,
+			TEvent trigger,
+			TStateId to,
+			Func<Transition<TStateId>, bool> condition = null,
+			bool forceInstantly = false)
+		{
+			fsm.AddTriggerTransitionFromAny(trigger, CreateOptimizedTransition(default, to, condition, forceInstantly));
+		}
+	}
+}
