9f66b7c28383fd8402c809bae13d9496acc949b4
Inspiaaa <lavaafterburner@gmail.com>
2022-02-27 17:13:04 +0100

    Improve error messages for cast error in actions

--- src/ActionState.cs
+++ src/ActionState.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using FSM.Exceptions;
 
 namespace FSM
 {
@@ -18,6 +19,31 @@ namespace FSM
 			actionsByEvent[trigger] = action;
 		}
 
+		private TTarget TryGetAndCastAction<TTarget>(TEvent trigger) where TTarget : Delegate {
+			Delegate action = null;
+			actionsByEvent?.TryGetValue(trigger, out action);
+
+			if (action is null) {
+				return null;
+			}
+
+			TTarget target = action as TTarget;
+
+			if (target is null) {
+				throw new InvalidOperationException(ExceptionFormatter.Format(
+					context: $"Trying to call the action '{trigger}'.",
+					problem: $"The expected argument type ({typeof(TTarget)}) does not match the "
+						+ $"type of the added action ({action}).",
+					solution: "Check that the type of action that was added matches the type of action that is called. \n"
+						+ "E.g. AddAction<int>(...) => OnAction<int>(...) \n"
+						+ "E.g. AddAction(...) => OnAction(...) \n"
+						+ "E.g. NOT: AddAction<int>(...) => OnAction<bool>(...)"
+				));
+			}
+
+			return target;
+		}
+
 		// Fluent interface
 		public ActionState<TStateId, TEvent> AddAction(TEvent trigger, Action action)
 		{
@@ -32,18 +58,10 @@ namespace FSM
 		}
 
 		public void OnAction(TEvent trigger)
-		{
-			Delegate action = null;
-			actionsByEvent?.TryGetValue(trigger, out action);
-			((Action)action)?.Invoke();
-		}
+			=> TryGetAndCastAction<Action>(trigger)?.Invoke();
 
 		public void OnAction<TData>(TEvent trigger, TData data)
-		{
-			Delegate action = null;
-			actionsByEvent?.TryGetValue(trigger, out action);
-			((Action<TData>)action)?.Invoke(data);
-		}
+			=> TryGetAndCastAction<Action<TData>>(trigger)?.Invoke(data);
 	}
 
 	public class ActionState : ActionState<string, string>
