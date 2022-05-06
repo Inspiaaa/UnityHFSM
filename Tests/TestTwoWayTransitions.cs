6fe43f31a5efb2aaf09c10f9326e0fc038a236a9
Inspiaaa <lavaafterburner@gmail.com>
2022-05-06 14:46:47 +0200

    Add basic unit tests for the two way transitions

---
+++ Tests/TestTwoWayTransitions.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using NUnit.Framework;
+
+namespace FSM.Tests
+{
+	public class TesTwoWayTransitions
+	{
+		private Recorder recorder;
+		private StateMachine fsm;
+
+		[SetUp]
+		public void Setup()
+		{
+			recorder = new Recorder();
+			fsm = new StateMachine();
+		}
+
+		[Test]
+		public void Test_two_way_transitions_work_both_ways()
+		{
+			bool shouldBeInB = false;
+
+			fsm.AddState("A", recorder.TrackedState);
+			fsm.AddState("B", recorder.TrackedState);
+			fsm.AddTwoWayTransition("A", "B", t => shouldBeInB);
+
+			fsm.Init();
+			fsm.OnLogic();
+			Assert.AreEqual("A", fsm.ActiveStateName);
+
+			shouldBeInB = true;
+			fsm.OnLogic();
+			Assert.AreEqual("B", fsm.ActiveStateName);
+
+			shouldBeInB = false;
+			fsm.OnLogic();
+			Assert.AreEqual("A", fsm.ActiveStateName);
+		}
+	}
+}
