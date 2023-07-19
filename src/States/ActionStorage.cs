9e57cb1f135432f86e638a27c3c4d77f1516c33e
Inspiaaa <lavaafterburner@gmail.com>
2023-07-20 00:29:13 +0200

    Change documentation comment indentation

--- src/States/ActionStorage.cs
+++ src/States/ActionStorage.cs
@@ -5,18 +5,18 @@ using FSM.Exceptions;
 namespace FSM
 {
 	/// <summary>
-	///  Class that can store and run actions.
-	///  It makes implementing an action system easier in the various state classes.
+	/// Class that can store and run actions.
+	/// It makes implementing an action system easier in the various state classes.
 	/// </summary>
 	public class ActionStorage<TEvent>
 	{
 		private Dictionary<TEvent, Delegate> actionsByEvent = new Dictionary<TEvent, Delegate>();
 
 		/// <summary>
-		///  Returns the action belonging to the specified event.
-		///  If it does not exist null is returned.
-		///  If the type of the existing action does not match the desired type an exception
-		///  is thrown.
+		/// Returns the action belonging to the specified event.
+		/// If it does not exist null is returned.
+		/// If the type of the existing action does not match the desired type an exception
+		/// is thrown.
 		/// </summary>
 		/// <param name="trigger">Name of the action</param>
 		/// <typeparam name="TTarget">Type of the function (delegate) belonging to the action</typeparam>
@@ -48,8 +48,8 @@ namespace FSM
 		}
 
 		/// <summary>
-		///  Adds an action that can be called with OnAction(). Actions are like the builtin events
-		///  OnEnter / OnLogic / ... but are defined by the user.
+		/// Adds an action that can be called with OnAction(). Actions are like the builtin events
+		/// OnEnter / OnLogic / ... but are defined by the user.
 		/// </summary>
 		/// <param name="trigger">Name of the action</param>
 		/// <param name="action">Function that should be called when the action is run</param>
@@ -59,8 +59,8 @@ namespace FSM
 		}
 
 		/// <summary>
-		///  Adds an action that can be called with RunAction<T>(). This overload allows you to
-		///  run a function that takes one data parameter.
+		/// Adds an action that can be called with RunAction<T>(). This overload allows you to
+		/// run a function that takes one data parameter.
 		/// </summary>
 		/// <param name="trigger">Name of the action</param>
 		/// <param name="action">Function that should be called when the action is run</param>
@@ -71,16 +71,16 @@ namespace FSM
 		}
 
 		/// <summary>
-		///  Runs an action with the given name.
-		///  If the action is not defined / hasn't been added, nothing will happen.
+		/// Runs an action with the given name.
+		/// If the action is not defined / hasn't been added, nothing will happen.
 		/// </summary>
 		/// <param name="trigger">Name of the action</param>
 		public void RunAction(TEvent trigger)
 			=> TryGetAndCastAction<Action>(trigger)?.Invoke();
 
 		/// <summary>
-		///  Runs an action with a given name and lets you pass in one parameter to the action function.
-		///  If the action is not defined / hasn't been added, nothing will happen.
+		/// Runs an action with a given name and lets you pass in one parameter to the action function.
+		/// If the action is not defined / hasn't been added, nothing will happen.
 		/// </summary>
 		/// <param name="trigger">Name of the action</param>
 		/// <param name="data">Data to pass as the first parameter to the action</param>
