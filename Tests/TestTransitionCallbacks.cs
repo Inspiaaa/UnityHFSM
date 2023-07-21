9ceebeb6d0988eb2c79cfd0326dc718f50756438
Inspiaaa <lavaafterburner@gmail.com>
2023-07-22 00:13:43 +0200

    Improve transition callbacks unit tests

--- Tests/TestTransitionCallbacks.cs
+++ Tests/TestTransitionCallbacks.cs
@@ -40,8 +40,64 @@ namespace FSM.Tests
 				.All();
 		}
 
-		// TODO: Pending transition
-		// TODO: With ghost state
+		[Test]
+		public void Test_transition_callbacks_work_in_flat_fsm_with_delayed_transition()
+		{
+			fsm.AddState("A", recorder.Track(new State(needsExitTime: true)));
+			fsm.AddState("B", recorder.TrackedState);
+
+			fsm.AddTransition("A", "B",
+				onTransition: t => recorder.RecordCustom("CallbackBefore"),
+				afterTransition: t => recorder.RecordCustom("CallbackAfter")
+			);
+
+			fsm.Init();
+			recorder.DiscardAll();
+
+			fsm.OnLogic();
+			recorder.Expect.Logic("A").All();
+
+			fsm.StateCanExit();
+			recorder.Expect
+				.Custom("CallbackBefore")
+				.Exit("A")
+				.Enter("B")
+				.Custom("CallbackAfter")
+				.All();
+		}
+
+		[Test]
+		public void Test_transition_callbacks_work_in_flat_fsm_with_ghost_state()
+		{
+			fsm.AddState("A", recorder.TrackedState);
+			fsm.AddState("B", recorder.Track(new State(isGhostState: true)));
+			fsm.AddState("C", recorder.TrackedState);
+
+			fsm.AddTransition("A", "B",
+				onTransition: t => recorder.RecordCustom("A->B Before"),
+				afterTransition: t => recorder.RecordCustom("A->B After")
+			);
+			fsm.AddTransition("B", "C",
+				onTransition: t => recorder.RecordCustom("B->C Before"),
+				afterTransition: t => recorder.RecordCustom("B->C After")
+			);
+
+			fsm.Init();
+			recorder.DiscardAll();
+
+			fsm.OnLogic();
+			recorder.Expect
+				.Custom("A->B Before")
+				.Exit("A")
+				.Enter("B")
+				.Custom("A->B After")
+				.Custom("B->C Before")
+				.Exit("B")
+				.Enter("C")
+				.Custom("B->C After")
+				.Logic("C")
+				.All();
+		}
 
 		[Test]
 		public void Test_transition_callbacks_work_in_nested_fsm_with_exit_transition()
