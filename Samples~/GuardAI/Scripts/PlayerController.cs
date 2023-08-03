af6a077d0877d5f0ed7aa863833aadc968c940e6
Inspiaaa <lavaafterburner@gmail.com>
2023-08-03 12:56:44 +0200

    Create guard AI sample

---
+++ Samples~/GuardAI/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FSM.Samples.GuardAI {
+
+public class PlayerController : MonoBehaviour
+{
+    public static PlayerController Instance { get; private set; }
+
+    public float speed = 5;
+
+    void Start()
+    {
+        Instance = this;
+        Application.targetFrameRate = 60;
+    }
+
+    void Update()
+    {
+        Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        input = Vector2.ClampMagnitude(input, 1);
+        input *= speed;
+
+        transform.position += (Vector3) (input * Time.deltaTime);
+    }
+}
+
+}
