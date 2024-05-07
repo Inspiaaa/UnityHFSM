21fb644658d7f8c640337f3ffc749ce4135cef76
Inspiaaa <inspiaaa@gmail.com>
2024-05-07 15:07:24 +0200

    Improve ease of use of ParallelStates
    Allow one to pass in the states that are to be run in parallel directly in the constructor of the ParallelStates class. Note that these then become nameless states, which will not be included in the active hierarchy path.

--- src/States/ParallelStates.cs
+++ src/States/ParallelStates.cs
@@ -10,6 +10,9 @@ namespace UnityHFSM
 	{
 		private List<StateBase<TStateId>> states = new List<StateBase<TStateId>>();
 
+		// When the states are passed in via the constructor, they are not assigned names / identifiers.
+		// This means that the active hierarchy path cannot include them (which would be used for debugging purposes).
+		private bool areStatesNameless = false;
 		private bool isActive;
 
 		private Func<ParallelStates<TOwnId, TStateId, TEvent>, bool> canExit;
@@ -23,6 +26,31 @@ namespace UnityHFSM
 			bool isGhostState = false) : base(needsExitTime, isGhostState)
 		{
 			this.canExit = canExit;
+		}
+
+		public ParallelStates(params StateBase<TStateId>[] states)
+			: this(null, false, false, states) { }
+
+		public ParallelStates(bool needsExitTime, params StateBase<TStateId>[] states)
+			: this(null, needsExitTime, false, states) { }
+
+		public ParallelStates(
+			Func<ParallelStates<TOwnId, TStateId, TEvent>, bool> canExit,
+			bool needsExitTime,
+			params StateBase<TStateId>[] states) : this(canExit, needsExitTime, false, states) { }
+
+		public ParallelStates(
+			Func<ParallelStates<TOwnId, TStateId, TEvent>, bool> canExit,
+			bool needsExitTime,
+			bool isGhostState,
+			params StateBase<TStateId>[] states) : base(needsExitTime, isGhostState)
+		{
+			this.canExit = canExit;
+			this.areStatesNameless = true;
+
+			foreach (var state in states) {
+				AddState(default, state);
+			}
 		}
 
 		public ParallelStates<TOwnId, TStateId, TEvent> AddState(TStateId id, StateBase<TStateId> state)
@@ -123,7 +151,11 @@ namespace UnityHFSM
 
 		public override string GetActiveHierarchyPath()
 		{
-			// Example path: "(Move & Attack/Shoot)"
+			if (areStatesNameless) {
+				return name.ToString();
+			}
+
+			// Example path: "Parallel/(Move & Attack/Shoot)"
 			string path = name.ToString() + "/(";
 
 			for (int i = 0; i < states.Count; i ++) {
@@ -143,6 +175,23 @@ namespace UnityHFSM
 			Func<ParallelStates<TStateId, TStateId, TEvent>, bool> canExit = null,
 			bool needsExitTime = false,
 			bool isGhostState = false) : base(canExit, needsExitTime, isGhostState) { }
+
+		public ParallelStates(params StateBase<TStateId>[] states)
+			: base(null, false, false, states) { }
+
+		public ParallelStates(bool needsExitTime, params StateBase<TStateId>[] states)
+			: base(null, needsExitTime, false, states) { }
+
+		public ParallelStates(
+			Func<ParallelStates<TStateId, TStateId, TEvent>, bool> canExit,
+			bool needsExitTime,
+			params StateBase<TStateId>[] states) : base(canExit, needsExitTime, false, states) { }
+
+		public ParallelStates(
+			Func<ParallelStates<TStateId, TStateId, TEvent>, bool> canExit,
+			bool needsExitTime,
+			bool isGhostState,
+			params StateBase<TStateId>[] states) : base(canExit, needsExitTime, isGhostState, states) { }
 	}
 
 	public class ParallelStates<TStateId> : ParallelStates<TStateId, TStateId, string>
@@ -151,6 +200,23 @@ namespace UnityHFSM
 			Func<ParallelStates<TStateId, TStateId, string>, bool> canExit = null,
 			bool needsExitTime = false,
 			bool isGhostState = false) : base(canExit, needsExitTime, isGhostState) { }
+
+		public ParallelStates(params StateBase<TStateId>[] states)
+			: base(null, false, false, states) { }
+
+		public ParallelStates(bool needsExitTime, params StateBase<TStateId>[] states)
+			: base(null, needsExitTime, false, states) { }
+
+		public ParallelStates(
+			Func<ParallelStates<TStateId, TStateId, string>, bool> canExit,
+			bool needsExitTime,
+			params StateBase<TStateId>[] states) : base(canExit, needsExitTime, false, states) { }
+
+		public ParallelStates(
+			Func<ParallelStates<TStateId, TStateId, string>, bool> canExit,
+			bool needsExitTime,
+			bool isGhostState,
+			params StateBase<TStateId>[] states) : base(canExit, needsExitTime, isGhostState, states) { }
 	}
 
 	public class ParallelStates : ParallelStates<string, string, string>
